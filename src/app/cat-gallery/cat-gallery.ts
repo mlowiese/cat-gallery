55e92d4e44f852813088373c7d55a1dd4ff04f4d
Maiza Louise <maizalouise@gmail.com>
2025-06-22 11:29:13 -0300

    new build anc fixes on first page.

--- src/app/cat-gallery/cat-gallery.ts
+++ src/app/cat-gallery/cat-gallery.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { CatService } from '../core/services/cat';
 import { CommonModule } from '@angular/common';
 
@@ -16,23 +16,32 @@ export class CatGallery implements OnInit {
   limit = 8;
   loading = false;
 
-  constructor(private catService: CatService) {}
-
+  constructor(
+    private catService: CatService,
+    private cdr: ChangeDetectorRef
+  ) {}
   ngOnInit() {
     this.loadMore();
   }
 
-  loadMore() {
-    this.loading = true;
-    this.catService.getCats(this.limit).subscribe(data => {
+loadMore() {
+  this.loading = true;
+  this.catService.getCats(this.limit).subscribe({
+    next: data => {
       this.cats = [...this.cats, ...data];
       if (!this.highlightedCat && this.cats.length > 0) {
         this.highlightedCat = this.cats[0];
       }
       this.page++;
       this.loading = false;
-    });
-  }
+      this.cdr.markForCheck();
+    },
+    error: err => {
+      this.loading = false;
+      this.cdr.markForCheck();
+    }
+  });
+}
 
   highlight(cat: any) {
     this.highlightedCat = cat;
